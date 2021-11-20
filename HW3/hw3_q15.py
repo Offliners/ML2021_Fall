4e3f58706a6d0a962001f10cf1f263c7af2f41c9
Offliner <40673034h@ntnu.edu.tw>
2021-11-21 04:45:24 +0800

    Create hw3_q15.py

---
+++ HW3/hw3_q15.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+train_filePath = 'hw3_train.dat'
+test_filePath = 'hw3_test.dat'
+
+def readFile(path):
+    X = []
+    y = []
+    for line in open(path).readlines():
+        data = line.strip().split('\t')
+        y.append(float(data[-1]))
+        X.append(list(map(float, data[:-1])))
+
+    return np.array(X), np.array(y)
+
+def transform(data, dim) :              
+    data_tf = np.column_stack(((np.ones(len(data)), data[:, list(range(dim))])))
+    
+    return data_tf
+
+def LinearRegression(X, y):
+    dagger = np.linalg.pinv(X)
+    wlin = np.matmul(dagger, y)
+    pred = np.matmul(X, wlin)
+    
+    return pred, wlin
+
+def ZeroOneError(X_pred, y):
+    pred = [np.sign(e) for e in X_pred]
+    y_pred = [np.sign(e) for e in pred]
+    err = np.mean(y != y_pred)
+
+    return err
+
+X_train, y_train = readFile(train_filePath)
+X_test, y_test = readFile(test_filePath)
+
+minError = np.Inf
+order = 0
+for i in range(10):
+    X_train_tf = transform(X_train, i + 1)
+    X_test_tf = transform(X_test, i + 1)
+
+    Pred_train, wlin = LinearRegression(X_train_tf, y_train)
+    Ein = ZeroOneError(Pred_train, y_train)
+
+    Pred_test = np.matmul(X_test_tf, wlin)
+    Eout = ZeroOneError(Pred_test, y_test)
+
+    err = abs(Ein - Eout)
+    print(f"Order {i + 1} : {err}")
+
+    if err < minError:
+        minError = err
+        order = i + 1
+
+print(f"Min error order is {order}")
