716ef2a1c75b8540233d2db7e7f555ee314e1981
Offliner <40673034h@ntnu.edu.tw>
2021-12-06 03:08:33 +0800

    Update hw4_q12.py

--- HW4/hw4_q12.py
+++ HW4/hw4_q12.py
@@ -1,5 +1,6 @@
 import numpy as np
 from liblinear.liblinearutil import *
+from itertools import combinations_with_replacement
 
 train_filePath = 'hw4_train.dat'
 test_filePath = 'hw4_test.dat'
@@ -16,27 +17,23 @@ def readFile(path):
 
 def transform(data, Q) : 
     data_tf = np.column_stack((np.ones(len(data)), data))
-    for i in range(2, Q + 1): 
-        idx = set(range(len(data[0])))
-        for j in range(len(data[0])):
-            idx = idx - {j}
-            temp = np.transpose(data[:, list(idx)])
-            first = np.power(data[:, j], i - 1)
-            for col in temp:
-                data_tf = np.column_stack((data_tf, np.multiply(first, col)))
-
-        exp = np.power(data, i)                  
-        data_tf = np.column_stack((data_tf, exp))
-    
+    c = range(len(data[0]))
+    for i in range(2, Q + 1):
+        orderList = [e for e in combinations_with_replacement(c, i)]
+        for j in orderList:
+            temp = np.ones(len(data))
+            for k in range(len(j)):
+                temp = np.multiply(temp, data[:, j[k]])
+            
+            data_tf = np.column_stack((data_tf, temp))
+        
     return data_tf
 
 def ZeroOneError(X_pred, y):
-    pred = [np.sign(e) for e in X_pred]
-    y_pred = [np.sign(e) for e in pred]
-    err = np.mean(y != y_pred)
+    err = np.mean(y != X_pred)
 
     return err
-  
+
 X_train, y_train = readFile(train_filePath)
 X_test, y_test = readFile(test_filePath)
 
@@ -49,15 +46,15 @@ print(X_test_tf.shape)
 minEout = np.inf
 best_log10_lambda = 0
 for log10_lambda in (-4, -2, 0, 2, 4):
-    c = 0.5 * (1 / 10 ** log10_lambda)
+    c = 1 / 2 / (10 ** log10_lambda)
     model = train(y_train, X_train_tf, '-s 0 -c ' + str(c) + ' -e 0.000001')
     Pred_test, _, _ = predict(y_test, X_test_tf, model)
-    Eout = ZeroOneError(y_test, Pred_test)
-     
-    if Eout < minEout:
+    Eout = ZeroOneError(Pred_test, y_test)
+
+    if Eout == minEout:
+        best_log10_lambda = max(best_log10_lambda, log10_lambda)
+    elif Eout < minEout:
         minEout = Eout
         best_log10_lambda = log10_lambda
-    else:
-        best_log10_lambda = max(best_log10_lambda, log10_lambda)
 
 print('The best log_10(λ*) = ', best_log10_lambda)
