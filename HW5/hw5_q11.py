398c9b5235277623bb7f266fc4cb6b55d944d842
Offliner <40673034h@ntnu.edu.tw>
2022-01-06 01:46:03 +0800

    Update hw5_q11.py

--- HW5/hw5_q11.py
+++ HW5/hw5_q11.py
@@ -8,21 +8,19 @@ def relabel(dataset, target):
         new_dataset.append(1 if item == target else -1)
     
     return new_dataset
-
-def sparse_matrix_dense(dataset, lower, upper):
-    dense = [dataset.get(item) for item in range(lower, upper + 1)]
-    return np.array([0 if item == None else item for item in dense])
   
 train_filePath = 'satimage.scale'
 
 y_train, X_train = svm_read_problem(train_filePath)
-upper = max(X_train[0].keys())
-lower = min(X_train[0].keys())
+X_upper = max(X_train[0].keys())
+X_lower = min(X_train[0].keys())
 new_y_train = relabel(y_train, 5)
 
 model = svm_train(new_y_train, X_train, '-c 10 -t 0')
-w = np.zeros(upper)
-for i in range(model.l):
-    w += model.sv_coef[0][i] * sparse_matrix_dense(X_train[model.sv_indices[i] - 1], lower, upper)
+w = np.zeros(X_upper)
+for i in range(len(model.get_sv_coef())):
+    support_vector = [model.get_SV()[i].get(item) for item in range(X_lower, X_upper + 1)]
+    support_vector = np.array([0 if item == None else item for item in support_vector])
+    w += model.get_sv_coef()[i][0] * support_vector
 
 print(np.linalg.norm(w))
