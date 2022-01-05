b90b4ad0f125eeb6cae143247213d7e60f4d1572
Offliner <40673034h@ntnu.edu.tw>
2022-01-06 02:05:20 +0800

    Create hw5_q12.py

---
+++ HW5/hw5_q12.py
@@ -0,0 +1,34 @@
+import numpy as np
+from libsvm.svmutil import *
+
+def relabel(dataset, target):
+    new_dataset = []
+
+    for item in dataset:
+        new_dataset.append(1 if item == target else -1)
+    
+    return new_dataset
+  
+train_filePath = 'satimage.scale'
+
+y_train, X_train = svm_read_problem(train_filePath)
+X_upper = max(X_train[0].keys())
+X_lower = min(X_train[0].keys())
+
+maxEin = 0
+ans = 0
+for target in range(2, 7):
+    new_y_train = relabel(y_train, target)
+
+    model = svm_train(new_y_train, X_train, '-s 0 -c 10 -d 3 -t 1 -g 1 -r 1')
+    p_label, p_acc, p_val = svm_predict(new_y_train, X_train, model)
+    print(p_acc)
+    Ein = 100 - p_acc[0]
+    print(f'\"{target}\" versus \"not {target}\" : Ein = {Ein}')
+
+    if Ein > maxEin:
+        maxEin = Ein
+        ans = target
+
+print('=============================')
+print(f'{ans} reaches the max Ein')
